c96c9937c80fc445537d382f9f8ddfc51a4fc92e
krispea3 <christian.bracher@gmail.com>
2019-05-26 16:20:17 +0800

    implemented stock,portfolio,fundcheck,buy,sell. All OK

--- src/store.js
+++ src/store.js
@@ -6,7 +6,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     funds: 10000,
-    stocks: []
+    stocks: [],
+    portfolio: []
   },
   getters: {
     funds: state => {
@@ -14,16 +15,53 @@ export default new Vuex.Store({
     },
     stocks: state => {
       return state.stocks
+    },
+    portfolio: state => {
+      return state.portfolio
     }
   },
   mutations: {
     setStocks: (state, stocks) => {
       state.stocks = stocks
+    },
+    addPortfolio: (state, portfolio) => {
+      state.portfolio.push(portfolio)
+    },
+    setFunds: (state, newFunds) => {
+      state.funds = newFunds
+    },
+    sellStock: (state, stock) => {
+      const record = state.portfolio.find(element => element.id === stock.id)
+      if (record) {
+        if (record.quantity <= stock.quantity) {
+          state.portfolio.splice(state.portfolio.indexOf(record), 1)
+        } else {
+          record.quantity -= stock.quantity
+        }
+      }
+      state.funds += stock.price * stock.quantity
     }
   },
   actions: {
     initStocks: ({ commit }, stocks) => {
       commit('setStocks', stocks)
+    },
+    addStockToPortfolio: (context, portfolio) => {
+      // Check if id already in portfolio
+      // The constant record just holds a pointer tho the state.portfolio element when found. Updating it updates the state value
+      const record = context.state.portfolio.find(element => element.id === portfolio.id)
+      if (record) {
+        record.quantity += portfolio.quantity
+      } else {
+        context.commit('addPortfolio', portfolio)
+      }
+      // Update funds
+      const value = portfolio.price * portfolio.quantity
+      const newFunds = context.state.funds - value
+      context.commit('setFunds', newFunds)
+    },
+    sellStock: ({ commit }, stock) => {
+      commit('sellStock', stock)
     }
   }
 })
---
+++ src/views/Portfolios.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-row>
+      <app-portfolio v-for = "portfolio in getPortfolio" :key="portfolio.id" :portfolio="portfolio"></app-portfolio>
+    </b-row>
+</template>
+
+<script>
+import Portfolio from '../components/Portfolio'
+
+export default {
+  components: {
+    'appPortfolio': Portfolio
+  },
+  computed: {
+    getPortfolio () {
+      return this.$store.getters.portfolio
+    }
+  }
+}
+</script>
